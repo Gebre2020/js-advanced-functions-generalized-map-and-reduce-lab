54ef551f507da603503bde926f126c4173913980
Gebremeskel <gebre20122@gmail.com>
2021-09-15 14:05:22 -0500

    commit tree c5eadc3e945233d20edf1cc9714899888fd49451

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for(let i=0; i<array.length; i++){
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(obj, callback, accumulator) {
+    const keys = Object.keys(obj)
+    let i = 1
+  
+    if (accumulator) {
+      i = 0
+    } else {
+      accumulator = obj[keys[0]]
+    }
+  
+    for (i; i < keys.length; i++) {
+      accumulator = callback(accumulator, obj[keys[i]])
+    }
+    return accumulator
+  }
